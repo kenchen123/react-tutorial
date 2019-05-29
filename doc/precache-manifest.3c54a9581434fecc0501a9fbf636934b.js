f0ee0a519d7c6b2407906116907a605c5e2af44e
ken <kenchen@pneumasoftware.com>
2019-05-29 14:29:17 +0800

    doc

---
+++ doc/precache-manifest.3c54a9581434fecc0501a9fbf636934b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41563f6689a43ab6308f3971d223e953",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "82580ee707d3c46f0432",
+    "url": "/react-tutorial/static/css/main.251767c1.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-tutorial/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "82580ee707d3c46f0432",
+    "url": "/react-tutorial/static/js/main.8aef05ad.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
